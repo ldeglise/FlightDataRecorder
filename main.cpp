2d17d533f9baf7f4b06d04ab0267eabdeea29eb6
ldeglise <ldeglise50@gmail.com>
2026-08-05 10:49:20 +0200

    Ajouter le fichier main.cpp avec les en-têtes SDK X-Plane

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+#include "SDK/CHeaders/XPLM/XPLMProcessing.h"
+#include "SDK/CHeaders/XPLM/XPLMDataAccess.h"
+#include "SDK/CHeaders/XPLM/XPLMUtilities.h"
+
+
